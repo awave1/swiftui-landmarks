6451ed1c70736a8dd23bbba04b42da352719807d
Artem Golovin <artgolovin96@gmail.com>
2019-10-12 21:19:27 -0600

    add search

--- Landmarks/pages/LandmarkList.swift
+++ Landmarks/pages/LandmarkList.swift
@@ -9,15 +9,24 @@ import SwiftUI
 
 struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
+    @State var searchString = ""
+
 
     var body: some View {
         NavigationView {
             List {
+                TextField ("Search", text: $searchString)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
                 Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
-
-                ForEach(userData.landmarks) { landmark in
+                
+                ForEach(
+                    searchString.isEmpty
+                        ? userData.landmarks
+                        : userData.landmarks.filter { searchFor(landmark: $0) }
+                ) { landmark in
                     if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                             LandmarkRow(landmark: landmark)
@@ -26,6 +35,14 @@ struct LandmarkList: View {
                 }
             }.navigationBarTitle(Text("Landmarks"))
         }
+    }
+
+    func searchFor(landmark: Landmark) -> Bool {
+        let name = landmark.name.lowercased()
+        let state = landmark.state.lowercased()
+        let search = self.searchString.lowercased()
+
+        return name.contains(search) || state.contains(search)
     }
 }
 
